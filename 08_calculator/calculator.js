71fbd9439de4072737f3d59431119df09381e328
John <kooymj@gmail.com>
2022-07-20 15:43:12 -0700

    Completed calculator.js and tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+// const sum = function(numbers) {
+// 	return numbers.reduce((a, b) => a + b, 0);
+// };
+const sum = function(numbers){
+  let total = 0;
+  for(let i = 0; i < numbers.length; i++){
+    total += numbers[i];
+  };
+  return total;
+}
+
+const multiply = function(numbers) {
+  let total = numbers[0];
+  for(let i = 1; i < numbers.length; i++){
+    total *= numbers[i];
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(a) {
+	let total = a;
+  if(a === 0){
+    return 1;
+  }
+  for(let i = a - 1; i >= 1; i--){
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
