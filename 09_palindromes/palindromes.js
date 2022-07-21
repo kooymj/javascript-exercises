f2271ff7bda9bf972cbb069525c3a8a184801f61
John <kooymj@gmail.com>
2022-07-20 19:07:13 -0700

    Finsihed palindromes and fibonacci

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    var reg = /[\W_]/g;
+    var lowerCase = str.toLowerCase().replace(reg, '');
+    
+    var reverseStr = lowerCase.split('').reverse().join('');
 
+    return reverseStr === lowerCase;
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(nth) {
+    if(nth < 0){
+        return 'OOPS';
+    }
+    if(isNaN(nth)){
+        nth = Number(nth);
+    }
+    let previousNum = 0;
+    let thisNum = 1;
+    let nextNum = 1;
+    for(let i = 1; i < nth; i++){
+        nextNum = previousNum + thisNum;
+        previousNum = thisNum;
+        thisNum = nextNum;
+    }
+    return nextNum;
 };
 
 // Do not edit below this line
