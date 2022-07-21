518cea02824239de5f96238193a6ce95ce58172d
John <kooymj@gmail.com>
2022-07-20 20:26:51 -0700

    completed exercises 11 and 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(objs) {
+    const oldest = objs.sort(function(a, b){
+        const lastGuy = a.yearOfDeath - a.yearOfBirth;
+        const nextGuy = b.yearOfDeath - b.yearOfBirth;
+        return lastGuy > nextGuy ? -1 : 1;
+    });
+    
+    if(!oldest[0].hasOwnProperty('yearOfDeath')){
+        return oldest[1];
+    }
+    return oldest[0];
 
+    
 };
 
 // Do not edit below this line
